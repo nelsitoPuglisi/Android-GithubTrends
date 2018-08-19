2d1dcf1026404faaa2da413025de57a583dbfe62
Nelson Puglisi <nelson.puglisi@mercadolibre.com>
2018-08-19 01:50:39 -0300

    github api implemented

--- app/src/main/kotlin/com/nelsito/githubtrends/data/GithubApi.kt
+++ app/src/main/kotlin/com/nelsito/githubtrends/data/GithubApi.kt
@@ -1,8 +1,24 @@
 package com.nelsito.githubtrends.data
 
+import com.nelsito.githubtrends.model.GithubRepo
+import com.nelsito.githubtrends.model.GithubUser
+import com.nelsito.githubtrends.model.TrendingGithub
+import com.nelsito.githubtrends.usecase.TrendingGithubListRepository
+import io.reactivex.Observable
 import retrofit2.Retrofit
 
-class GithubApi {
+class GithubApi : TrendingGithubListRepository {
+    override fun load(): Observable<TrendingGithub> {
+        return service.getTrendingRepositories()
+                .map {
+                    TrendingGithub(
+                        it.items.map {
+                            GithubRepo(GithubUser(it.owner.login, it.owner.id.toString(), it.owner.avatarUrl), it.name, it.id.toString())
+                        }
+                    )
+                }
+    }
+
     private var service: GithubService
 
     init {
